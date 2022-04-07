661949bfc04a1f0a565b84e73e9ba4a5eede162e
Piotr Bilski <bilski.piotr@outlook.com>
2022-04-07 20:37:29 +0200

    Poprawiono kod

--- PO-W-503/Program.cs
+++ PO-W-503/Program.cs
@@ -4,7 +4,6 @@ using System;
 
 namespace PO_W_503
 {
-    // ======== 1 ========
     struct Values
     {
         int _value;
@@ -49,49 +48,4 @@ namespace PO_W_503
             Console.ReadKey();
         }
     }
-
-    // ======== 2 ========
-    //class Values
-    //{
-    //    int _value;
-    //    // konstruktor struktury
-    //    public Values(int value)
-    //    {
-    //        _value = value;
-    //    }
-    //    // właściwość podająca wartość pola prywatnego
-    //    public int Wartosc
-    //    {
-    //        get { return (_value); }
-    //    }
-    //    // nadpisanie operatora (--)
-    //    public static Values operator --(Values par)
-    //    {
-    //        return new Values(par._value - 3);
-    //    }
-    //    // nadpisanie operatora(++)
-    //    public static Values operator ++(Values par)
-    //    {
-    //        return new Values(par._value + 3);
-    //    }
-    //}
-
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        Values v1 = new Values(10);
-    //        Values v2 = ++v1;
-    //        Console.WriteLine("Inkrementacja prefiksowa : {0}", v2.Wartosc);
-    //        Values v3 = --v1;
-    //        Console.WriteLine("Dekrementacja prefiksowa : {0}", v3.Wartosc);
-    //        v1 = new Values(10);
-    //        Console.WriteLine("Aktualna wartosc v1 = {0}", v1.Wartosc);
-    //        Values v4 = v1++;
-    //        Console.WriteLine("Inkrementacja postfiksowa : {0}", v4.Wartosc);
-    //        Values v5 = v1--;
-    //        Console.WriteLine("Dekrementacja postfiksowa : {0}", v5.Wartosc);
-    //        Console.ReadKey();
-    //    }
-    //}
 }
